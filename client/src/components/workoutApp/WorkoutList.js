657fe37fcc420005850affc044d5ae7d83ea9cfb
Josh Holtkort <jholtkort@utexas.edu>
2019-09-25 14:01:49 -0500

    fixed navbar and loading

--- client/src/components/workoutApp/WorkoutList.js
+++ client/src/components/workoutApp/WorkoutList.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 import Workout from "./Workout";
 
 class WorkoutList extends Component {
-  state = { workouts: [] };
+  state = { workouts: [], loaded: false };
 
   componentDidMount = async () => {
     await axios
       .get("http://localhost:4000/workouts")
       .then(res => {
-        this.setState({ workouts: res.data });
+        this.setState({ workouts: res.data, loaded: true });
       })
       .catch(function(error) {
         console.log(error);
@@ -43,7 +44,18 @@ class WorkoutList extends Component {
   };
 
   render() {
-    if (!this.state.workouts.length) return <div>Loading...</div>;
+    if (!this.state.workouts.length && !this.state.loaded) {
+      return <div>Loading...</div>;
+    }
+
+    if (!this.state.workouts.length && this.state.loaded) {
+      return (
+        <div>
+          <Link to="/create">Please add workouts</Link>
+        </div>
+      );
+    }
+
     return <div>{this.renderWorkouts()}</div>;
   }
 }
