70c67089e14918cf6747f426d1d1b77984bb384e
Josh Holtkort <jholtkort@utexas.edu>
2019-10-17 12:10:05 -0500

    added workout model

--- api/routes/workouts.js
+++ api/routes/workouts.js
@@ -1,60 +1,6 @@
-const Joi = require("@hapi/joi");
-const mongoose = require("mongoose");
+const { validate, Workout } = require("../models/workout");
 const express = require("express");
 const router = express.Router();
-
-const workoutSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    minlength: 5,
-    maxlength: 255,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  type: {
-    type: String,
-    enum: ["swim", "bike", "run"],
-    required: true
-  },
-  time: {
-    type: String,
-    required: true
-  },
-  distance: {
-    type: Number,
-    min: 1,
-    max: 500,
-    required: true
-  },
-  distanceUnits: {
-    type: String,
-    enum: ["mi", "yd", "km", "m"],
-    required: true
-  },
-  hour: {
-    type: Number,
-    min: 0,
-    max: 99,
-    required: true
-  },
-  minute: {
-    type: Number,
-    min: 0,
-    max: 59,
-    required: true
-  },
-  second: {
-    type: Number,
-    min: 0,
-    max: 59,
-    required: true
-  }
-});
-
-const Workout = mongoose.model("Workout", workoutSchema);
 
 router.get("/", async (req, res) => {
   const workouts = await Workout.find().sort("date");
@@ -62,7 +8,7 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateWorkout(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let workout = new Workout({
@@ -83,7 +29,7 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateWorkout(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const workout = await Workout.findByIdAndUpdate(
@@ -125,42 +71,5 @@ router.get("/:id", async (req, res) => {
 
   res.send(workout);
 });
-
-function validateWorkout(workout) {
-  const schema = Joi.object({
-    description: Joi.string()
-      .min(5)
-      .max(255)
-      .required(),
-    date: Joi.date().required(),
-    type: Joi.string()
-      .pattern(/(swim|bike|run)/)
-      .required(),
-    time: Joi.string()
-      .pattern(/\b((1[0-2]|0?[1-9]):([0-5][0-9])([AaPp][Mm]))/)
-      .required(),
-    distance: Joi.number()
-      .min(1)
-      .max(500)
-      .required(),
-    distanceUnits: Joi.string()
-      .pattern(/(mi|yd|km|m)/)
-      .required(),
-    hour: Joi.number()
-      .min(0)
-      .max(99)
-      .required(),
-    minute: Joi.number()
-      .min(0)
-      .max(59)
-      .required(),
-    second: Joi.number()
-      .min(0)
-      .max(59)
-      .required()
-  });
-
-  return schema.validate(workout);
-}
 
 module.exports = router;
