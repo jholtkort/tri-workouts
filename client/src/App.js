7e96bc30dd0c7d13fb37fa22d18af6be3591960a
Josh Holtkort <jholtkort@utexas.edu>
2019-11-04 14:58:52 -0600

    added custom history to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 
@@ -9,11 +9,12 @@ import Navbar from "./components/workoutApp/Navbar";
 import WorkoutList from "./components/workoutApp/WorkoutList";
 import CreateWorkout from "./components/workoutApp/CreateWorkout";
 import UpdateWorkout from "./components/workoutApp/UpdateWorkout";
+import createBrowserHistory from "./history";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Router>
+      <Router history={createBrowserHistory}>
         <Navbar />
         <div className="main-app">
           <Switch>
