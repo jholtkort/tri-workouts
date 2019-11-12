370bab73578e7aa1e1a119ab7e6c24a1138a6365
Josh Holtkort <jholtkort@utexas.edu>
2019-11-12 16:12:57 -0600

    added authReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
 
+import authReducer from "./authReducer";
 import workoutReducer from "./workoutReducer";
 
 export default combineReducers({
+  auth: authReducer,
   workout: workoutReducer,
   form: formReducer
 });
