7a20baf95590717488b27a505ac3447323a4b748
Josh Holtkort <jholtkort@utexas.edu>
2019-11-12 16:12:22 -0600

    added authentication to app component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { fetchUser } from "../actions/index";
 
 import "./App.css";
-import Header from "./workouts/Header";
+import Header from "./Header";
 import Landing from "./Landing";
 import WorkoutList from "./workouts/WorkoutList";
 import WorkoutCreate from "./workouts/WorkoutCreate";
 import WorkoutUpdate from "./workouts/WorkoutUpdate";
 import createBrowserHistory from "../history";
 
-const App = () => {
-  return (
-    <Router history={createBrowserHistory}>
-      <Header />
-      <div className="main-app my-5">
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route path="/workouts" component={WorkoutList} />
-          <Route path="/create" component={WorkoutCreate} />
-          <Route path="/update/:id" component={WorkoutUpdate} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <Router history={createBrowserHistory}>
+        <Header />
+        <div className="main-app my-5">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route path="/workouts" component={WorkoutList} />
+            <Route path="/create" component={WorkoutCreate} />
+            <Route path="/update/:id" component={WorkoutUpdate} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { fetchUser }
+)(App);
