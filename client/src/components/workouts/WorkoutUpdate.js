e9a793081d620ef8af853673c37cbdd020fa9bbf
Josh Holtkort <jholtkort@utexas.edu>
2019-11-14 15:08:22 -0600

    updated WorkoutUpdate for Redux Form

--- client/src/components/workouts/WorkoutUpdate.js
+++ client/src/components/workouts/WorkoutUpdate.js
@@ -3,26 +3,17 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import WorkoutForm from "./WorkoutForm";
-import {
-  getWorkoutById,
-  updateWorkout,
-  deleteWorkout
-} from "../../actions/workoutActions";
+import { getWorkoutById, updateWorkout } from "../../actions";
 
 class UpdateWorkout extends Component {
   static propTypes = {
     getWorkoutById: PropTypes.func.isRequired,
-    updateWorkout: PropTypes.func.isRequired,
-    deleteWorkout: PropTypes.func.isRequired
+    updateWorkout: PropTypes.func.isRequired
   };
 
   componentDidMount() {
     this.props.getWorkoutById(this.props.match.params.id);
   }
-
-  handleDeleteClick = () => {
-    this.props.deleteWorkout(this.props.match.params.id);
-  };
 
   onSubmit = formValues => {
     this.props.updateWorkout(this.props.match.params.id, formValues);
@@ -34,16 +25,15 @@ class UpdateWorkout extends Component {
         <div>
           <WorkoutForm
             onSubmit={this.onSubmit}
-            handleDeleteClick={this.handleDeleteClick}
             title="Update Workout"
-            description={this.props.workout.description}
-            date={this.props.workout.date}
-            type={this.props.workout.type}
-            distance={this.props.workout.distance}
-            distanceUnits={this.props.workout.distanceUnits}
-            hour={this.props.workout.hour}
-            minute={this.props.workout.minute}
-            second={this.props.workout.second}
+            // description={this.props.workout.description}
+            // date={this.props.workout.date}
+            // type={this.props.workout.type}
+            // distance={this.props.workout.distance}
+            // distanceUnits={this.props.workout.distanceUnits}
+            // hour={this.props.workout.hour}
+            // minute={this.props.workout.minute}
+            // second={this.props.workout.second}
           />
         </div>
       );
@@ -54,10 +44,9 @@ class UpdateWorkout extends Component {
 }
 
 const mapStateToProps = state => {
-  return { workout: state.workout.workouts };
+  return { workout: state.workout.workout };
 };
 
-export default connect(
-  mapStateToProps,
-  { getWorkoutById, updateWorkout, deleteWorkout }
-)(UpdateWorkout);
+export default connect(mapStateToProps, { getWorkoutById, updateWorkout })(
+  UpdateWorkout
+);
