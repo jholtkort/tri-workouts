51dd37f9100af62a611c80239bfffb0b635c29ba
Josh Holtkort <jholtkort@utexas.edu>
2019-11-16 14:51:36 -0600

    refactor Workout component to functional component

--- client/src/components/workouts/Workout.js
+++ client/src/components/workouts/Workout.js
@@ -1,75 +1,58 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Moment from "react-moment";
+import DeleteModal from "../DeleteModal";
 
 import { deleteWorkout } from "../../actions/workoutActions";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-  CardText,
-  Button
-} from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, CardText } from "reactstrap";
 
 import "./Workout.css";
 
-class Workout extends Component {
-  onDeleteClick = () => {
-    this.props.deleteWorkout(this.props.workout._id);
-  };
+const Workout = props => {
+  const {
+    _id,
+    description,
+    date,
+    type,
+    distance,
+    distanceUnits,
+    hour,
+    minute,
+    second
+  } = props.workout;
 
-  render() {
-    const {
-      _id,
-      description,
-      date,
-      type,
-      distance,
-      distanceUnits,
-      hour,
-      minute,
-      second
-    } = this.props.workout;
+  return (
+    <Card>
+      <CardBody>
+        <CardTitle>
+          <Link to={`/workouts/update/${_id}`} id="workout-title">
+            {description}
+          </Link>
+        </CardTitle>
+        <CardSubtitle className="mb-2 text-muted">
+          <Moment className="mr-1" format="MM/DD/YYYY">
+            {date}
+          </Moment>
+          at
+          <span className="ml-1">
+            <Moment format="h:mm a">{date}</Moment>
+          </span>
+        </CardSubtitle>
 
-    return (
-      <Card>
-        <CardBody>
-          <CardTitle>
-            <Link to={`/workouts/update/${_id}`} id="workout-title">
-              {description}
-            </Link>
-          </CardTitle>
-          <CardSubtitle className="mb-2 text-muted">
-            <Moment className="mr-1" format="MM/DD/YYYY">
-              {date}
-            </Moment>
-            at
-            <span className="ml-1">
-              <Moment format="h:mm a">{date}</Moment>
-            </span>
-          </CardSubtitle>
-
-          <CardText className="workout-stats">
-            <li className="pr-3 mr-3">{type}</li>
-            <li className="pr-3 mr-3">
-              {distance} {distanceUnits}
-            </li>
-            <li className="mr-3">
-              {hour} hr {minute} min {second} sec
-            </li>
-          </CardText>
-          <Button
-            className="waves-effect waves-light btn red"
-            onClick={this.onDeleteClick}
-          >
-            <i className="material-icons">delete</i>
-          </Button>
-        </CardBody>
-      </Card>
-    );
-  }
-}
+        <CardText className="workout-stats">
+          <li className="pr-3 mr-3">{type}</li>
+          <li className="pr-3 mr-3">
+            {distance} {distanceUnits}
+          </li>
+          <li className="mr-3">
+            {hour} hr {minute} min {second} sec
+          </li>
+        </CardText>
+        <DeleteModal id={_id} />
+      </CardBody>
+    </Card>
+  );
+};
 
 export default connect(null, { deleteWorkout })(Workout);
