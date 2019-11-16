5aebda5075aa7d73218ca7ac0aebe0cd89242028
Josh Holtkort <jholtkort@utexas.edu>
2019-11-15 18:32:28 -0600

    added name to Dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
 import WorkoutList from "./workouts/WorkoutList";
 
-const Dashboard = () => {
+const Dashboard = props => {
+  console.log(props);
   return (
     <div>
+      <h1 className="mb-3">Welcome, {props.auth.name}</h1>
       <WorkoutList />
       <div className="fixed-action-btn">
         <Link to="/workouts/create" className="btn-floating btn-large orange">
@@ -15,4 +19,8 @@ const Dashboard = () => {
   );
 };
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Dashboard);
