a8820a1d91fa987756a808d8cd849bf2f1513dee
Josh Holtkort <jholtkort@utexas.edu>
2019-11-27 08:11:11 -0600

    moved types.js to new folder

--- client/src/actions/types.js
+++
@@ -1,8 +0,0 @@
-export const GET_WORKOUTS = "GET_WORKOUTS";
-export const CREATE_WORKOUT = "ADD_WORKOUT";
-export const UPDATE_WORKOUT = "UPDATE_WORKOUT";
-export const DELETE_WORKOUT = "DELETE_WORKOUT";
-export const WORKOUTS_LOADING = "WORKOUTS_LOADING";
-export const GET_WORKOUT_BY_ID = "GET_WORKOUT_BY_ID";
-export const FETCH_USER = "FETCH_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
